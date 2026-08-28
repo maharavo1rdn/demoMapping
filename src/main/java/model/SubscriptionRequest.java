e8aee6fe585b9b06f65ebfe6f8216a35f454835d
RANDRIANOROSON Mitanasoa Maharavo <maharavordn@gmail.com>
2026-08-28 08:25:39 +0300

    Update SubscriptionRequest.java

--- src/main/java/model/SubscriptionRequest.java
+++ src/main/java/model/SubscriptionRequest.java
@@ -7,7 +7,8 @@ public class SubscriptionRequest {
     private String status;
     private String nextBillingDate;
 
-    public SubscriptionRequest(String externalRef, String planId, String customerMsisdn, String status, String nextBillingDate) {
+    public SubscriptionRequest(String externalRef, String planId, String customerMsisdn, String status,
+            String nextBillingDate) {
         this.externalRef = externalRef;
         this.planId = planId;
         this.customerMsisdn = customerMsisdn;
@@ -15,9 +16,43 @@ public class SubscriptionRequest {
         this.nextBillingDate = nextBillingDate;
     }
 
-    public String getExternalRef() { return externalRef; }
-    public String getPlanId() { return planId; }
-    public String getCustomerMsisdn() { return customerMsisdn; }
-    public String getStatus() { return status; }
-    public String getNextBillingDate() { return nextBillingDate; }
+    public String getExternalRef() {
+        return externalRef;
+    }
+
+    public String getPlanId() {
+        return planId;
+    }
+
+    public String getCustomerMsisdn() {
+        return customerMsisdn;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getNextBillingDate() {
+        return nextBillingDate;
+    }
+
+    public void setExternalRef(String externalRef) {
+        this.externalRef = externalRef;
+    }
+
+    public void setPlanId(String planId) {
+        this.planId = planId;
+    }
+
+    public void setCustomerMsisdn(String customerMsisdn) {
+        this.customerMsisdn = customerMsisdn;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setNextBillingDate(String nextBillingDate) {
+        this.nextBillingDate = nextBillingDate;
+    }
 }
